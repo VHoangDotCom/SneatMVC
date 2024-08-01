06530b41ba39de7bc4118c285b21234420cdc8a8
VHoangDotCom <viethoang2001gun@gmail.com>
2024-08-01 17:34:50 +0700

    finish role management

--- Sneat.MVC/Scripts/ScriptModel/role-script.js
+++ Sneat.MVC/Scripts/ScriptModel/role-script.js
@@ -76,3 +76,165 @@
         }
     });
 }
+
+function saveUpdateRole() {
+    var id = $("#roleID").val();
+    var name = $('#nameEdit').val();
+    var description = $('#txtDescriptionEdit').val();
+
+    var checkboxTree = $('#update-tree-role');
+    var selectedNodes = checkboxTree.jstree("get_selected", true);
+    var selectedNodeIds = selectedNodes.map(function (node) {
+        return node.id;
+    });
+
+    if (name == "") {
+        Swal.fire({
+            title: 'Thông báo!',
+            text: ' Vui lòng nhập tên vai trò!',
+            icon: 'warning',
+            customClass: {
+                confirmButton: 'btn btn-primary'
+            },
+            buttonsStyling: false
+        });
+        return;
+    }
+
+    $.ajax({
+        url: "/Roles/UpdateRole",
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            ID: id,
+            name: name,
+            description: description,
+            permissionIDs: selectedNodeIds
+        }),
+        beforeSend: function () {
+            $("#modalLoad").modal("show");
+        },
+        success: function (response) {
+            $("#modalLoad").modal("hide");
+            if (response == -1) {
+                Swal.fire({
+                    title: 'Không thể cập nhật vai trò',
+                    text: `Đã tồn tại vai trò có tên ${name}!`,
+                    icon: 'warning',
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    },
+                    buttonsStyling: false
+                });
+            }
+            else if (response == 1) {
+                Swal.fire({
+                    title: 'Thành công!',
+                    text: 'Cập nhật vai trò thành công!',
+                    icon: 'success',
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    },
+                    buttonsStyling: false
+                });
+                setTimeout(function () {
+                    window.location = "/Roles/Index?";
+                    //searchUser();
+                }, 2000);
+            }
+
+            else {
+                Swal.fire({
+                    title: 'Có lỗi xảy ra!',
+                    text: ' Không thể cập nhật vai trò!',
+                    icon: 'error',
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    },
+                    buttonsStyling: false
+                });
+            }
+        }
+    });
+}
+
+function deleteRole(id) {
+    if (!navigator.onLine) {
+        Swal.fire({
+            title: 'Có lỗi xảy ra!',
+            text: ' Kiểm tra kết nối internet!',
+            icon: 'error',
+            customClass: {
+                confirmButton: 'btn btn-primary'
+            },
+            buttonsStyling: false
+        });
+        return;
+    }
+    Swal.fire({
+        title: 'Bạn chắc chắn muốn xóa chứ?',
+        text: "Dữ liệu vai trò này sẽ bị xóa!",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Vâng, tôi xác nhận!',
+        customClass: {
+            confirmButton: 'btn btn-primary me-3',
+            cancelButton: 'btn btn-label-secondary'
+        },
+        buttonsStyling: false
+    }).then(function (resultConfirm) {
+        console.log(resultConfirm)
+        if (resultConfirm.isConfirmed) {
+            $.ajax({
+                url: '/Roles/RemoveRole',
+                data: { id: id },
+                type: "POST",
+                success: function (result) {
+                    if (result == 1) {
+                        Swal.fire({
+                            title: 'Thành công!',
+                            text: 'Xóa vai trò thành công!',
+                            icon: 'success',
+                            customClass: {
+                                confirmButton: 'btn btn-primary'
+                            },
+                            buttonsStyling: false
+                        });
+                        setTimeout(function () {
+                            window.location = "/Roles/Index?";
+                            //searchUser();
+                        }, 2000);
+                    } else if (result == -2) {
+                        Swal.fire({
+                            title: 'Xóa vai trò thất bại!',
+                            text: 'Vai trò hiện không tồn tại hoặc đã bị xóa!',
+                            icon: 'warning',
+                            customClass: {
+                                confirmButton: 'btn btn-primary'
+                            },
+                            buttonsStyling: false
+                        });
+                        setTimeout(
+                            function () {
+                                window.location = "/Roles/Index?";
+                                //searchUser();
+                            }, 1000);
+
+                    }
+                    else {
+                        Swal.fire({
+                            title: 'Có lỗi xảy ra!',
+                            text: ' Không thể xóa vai trò!',
+                            icon: 'error',
+                            customClass: {
+                                confirmButton: 'btn btn-primary'
+                            },
+                            buttonsStyling: false
+                        });
+                    }
+                }
+            });
+        }
+    });
+
+}
